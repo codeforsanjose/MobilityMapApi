1acbbe1666bc0d6ae222880bd1aedd0f9345fb3e
Wilson Wang <wilsonw@stanford.edu>
2015-08-02 18:16:29 -0700

    try some list data

--- src/sa_api_v2/views/mobility_views.py
+++ src/sa_api_v2/views/mobility_views.py
@@ -55,6 +55,11 @@ logger = logging.getLogger('sa_api_v2.views')
 # -------------------
 #
 
+vta = []
+
+# we want data in the form 'lat', 'long', 'name', 'url'
+vta.append(['33.755787', '-116.359998', 'VTA Construction', 'http://google.com'])
+vta.append(['33.755787', '-116.359998', 'VTA Construction', 'http://google.com'])
 
 def bulk(request="none"):
 	place = models.core.Place()
@@ -64,6 +69,6 @@ def bulk(request="none"):
 	my_long = '-116.359998'
 	place.geometry = fromstr('POINT(' + my_long + ' ' + my_lat + ')')
 	place.data = '{"private-submitter_email":"test","name":"test","user_token":"session:842e0c2fb637edb9787cb0678a16016d","hint_category":"improvement","location":{"sideOfStreet":"N","linkId":"0","mapUrl":"http:\/\/open.mapquestapi.com\/staticmap\/v4\/getmap?key=Fmjtd|luur2g0bnl,25=o5-9at29u&type=map&size=225,160&pois=purple-1,37.3440744,-121.8864022,0,0,|&center=37.3440744,-121.8864022&zoom=15&rand=860370430","displayLatLng":{"lat":37.344074,"lng":-121.886402},"adminArea6Type":"Neighborhood","adminArea3Type":"State","dragPoint":false,"geocodeQualityCode":"B1AAA","adminArea1":"US","geocodeQuality":"STREET","adminArea3":"CA","adminArea4":"Santa Clara County","adminArea5":"San Jos\u00e9","adminArea6":"","postalCode":"95112","type":"s","adminArea1Type":"Country","adminArea5Type":"City","latLng":{"lat":37.344074,"lng":-121.886402},"adminArea4Type":"County","unknownInput":"","street":"North 8th Street"},"submitter_name":"test","location_type":"side_walk","description":"test"}';
-	place.save()
+	# place.save()
 
-	return HttpResponse("Hello, world. You're at the polls index.")
+	return HttpResponse(vta[1])
